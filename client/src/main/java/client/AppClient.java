057c179f0b85a042d39689a8039b913eb1b27c6a
lai2-24 <lai2-24@itismeucci.com>
2023-10-13 11:34:59 +0200

    Added main in both

--- client/src/main/java/client/AppClient.java
+++ client/src/main/java/client/AppClient.java
@@ -4,10 +4,22 @@ package client;
  * Hello world!
  *
  */
-public class App 
+public class AppClient 
 {
+    static String serverInput;
+
+    static String address = "localhost";
+    static int port = 5678;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Client client = new Client();
+
+        client.start(address, port);
+
+        serverInput = client.receive();
+        System.out.println(serverInput);
+
+        client.send("Connessione riuscita");
+
     }
 }
--- server/src/main/java/server/AppServer.java
+++ server/src/main/java/server/AppServer.java
@@ -1,13 +1,65 @@
 package server;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ServerSocket;
+
+public class AppServer
 {
+    static int port = 5678;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ServerSocket serverSocket;
+        try{
+            serverSocket = new ServerSocket(port);
+        }catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        
+        
+        System.out.println("Il server e' aperto [porta "+port+"]");
+        for(;;) {
+            Server server = new Server();
+            server.accept(serverSocket);
+            System.out.println("Client connesso");
+            SendImg send = new SendImg(server);
+            send.start();
+        }
+    }
+
+    public static class SendImg extends Thread{
+
+        private Server server;
+
+        public SendImg(Server server){
+            this.server = server;
+        }
+
+        public void run(){
+            try{
+
+
+                File imgref = new File("../images/bluesmurfcat.jpg");
+                FileInputStream fin = new FileInputStream(imgref);
+                String len = ""+imgref.length();
+                server.send(len);
+
+
+
+            }catch(FileNotFoundException e){
+
+
+                System.out.println("Impossibile trovare il file richiesto.");
+                e.printStackTrace();
+
+
+            }
+            
+        }
+
+
     }
 }
