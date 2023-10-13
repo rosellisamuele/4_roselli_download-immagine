6de48d5b52fcff5ad5a6fcde0d134471bb7c6ec1
lai2-24 <lai2-24@itismeucci.com>
2023-10-13 12:39:35 +0200

    Attempted fixing

--- client/src/main/java/client/AppClient.java
+++ client/src/main/java/client/AppClient.java
@@ -9,17 +9,21 @@ public class AppClient
     static String serverInput;
 
     static String address = "localhost";
-    static int port = 5678;
+    static int port = 6789;
     public static void main( String[] args )
     {
         Client client = new Client();
 
         client.start(address, port);
 
+        
         serverInput = client.receive();
         System.out.println(serverInput);
 
         client.send("Connessione riuscita");
 
+        client.receive();
+        System.out.println(serverInput);
+    
     }
 }
--- server/src/main/java/server/AppServer.java
+++ server/src/main/java/server/AppServer.java
@@ -1,14 +1,18 @@
 package server;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class AppServer
 {
-    static int port = 5678;
+    static int port = 6789;
+
+    /* 
     public static void main( String[] args )
     {
         ServerSocket serverSocket;
@@ -22,30 +26,84 @@ public class AppServer
         
         System.out.println("Il server e' aperto [porta "+port+"]");
         for(;;) {
-            Server server = new Server();
-            server.accept(serverSocket);
-            System.out.println("Client connesso");
-            SendImg send = new SendImg(server);
-            send.start();
+            try{
+                Server server = new Server();
+                server.accept(serverSocket);
+                System.out.println("Client connesso");
+                SendImg send = new SendImg(server);
+                send.start();
+            }catch(Exception e){}
+            
         }
     }
+    */
+
+    public static void main(String[] args){
+        try{
+            ServerSocket serverSocket;
+            try{
+                serverSocket = new ServerSocket(port);
+            }catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+
+            System.out.println("Connessione accettata.");
+            
+            for(;;){
+                Server server = new Server();
+                server.accept(serverSocket);
+                SendImg img = new SendImg(server);
+                img.start();
+            }
+
+
+            
+
+
+
+
+
+            
+        }
+    }
+
 
     public static class SendImg extends Thread{
 
         private Server server;
+        protected static File file;
 
         public SendImg(Server server){
             this.server = server;
         }
 
+        @Override
         public void run(){
+            try{
+                FileInputStream reader = new FileInputStream("./images/bluesmurfcat.jpg");
+            }catch(IOException e){}
+
+            byte[] buffer = new byte[1024];
+            int readLen;
+            while((readLen = reader.read(buffer)) > 0){
+                writer.write(buffer, 0, readLen);
+            }
+            
+
+
+            /* 
             try{
 
 
-                File imgref = new File("../images/bluesmurfcat.jpg");
-                FileInputStream fin = new FileInputStream(imgref);
-                String len = ""+imgref.length();
+                System.out.println("Immagine trovata!");
+                String len = String.valueOf(file.length());
                 server.send(len);
+
+
+                byte[] bytes = fin.readAllBytes();
+                server.send(bytes);
+                
 
 
 
@@ -57,7 +115,7 @@ public class AppServer
 
 
             }
-            
+            */
         }
 
 
